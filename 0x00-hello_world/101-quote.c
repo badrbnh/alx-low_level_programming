ae5cee573a0c1f2cc700b091d6074efe5a4df8ca
badreddine <badrechahide@gmail.com>
2023-02-16 12:07:47 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,9 @@
  *
  * Return: Always 1 (Success)
  */
-#include <unistd.h>
-
 int main(void)
 {
-  const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(STDERR_FILENO, msg, sizeof(msg) - 1);
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, msg, sizeof(msg) - 1);
 	return (1);
 }
-
