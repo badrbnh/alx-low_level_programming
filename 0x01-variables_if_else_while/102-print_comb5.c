ae93745b559cb0d8c502a18a67c50a674e8a18c7
badreddine <badrechahide@gmail.com>
2023-02-19 08:03:02 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,11 +6,10 @@
 */
 int main(void)
 {
-	int i = 0, j;
+	int i = 0, j = 1;
 
 	while (i < 100)
 	{
-	j = i;
 	while (j < 100)
 	{
 		putchar(i / 10 + '0');
@@ -18,15 +17,15 @@ int main(void)
 		putchar(' ');
 		putchar(j / 10 + '0');
 		putchar(j % 10 + '0');
-		if (i != 99 || j != 99)
-		{
+		if (i != 98 || j != 99)
+	{
 		putchar(',');
 		putchar(' ');
 	}
 		j++;
 	}
 		i++;
+		j = i + 1;
 	}
-	putchar('\n');
-	return (0);
+		return (0);
 }
