8fa3861b7c122fd1de9532fbfa3d5a760fd639e4
badreddine <badrechahide@gmail.com>
2023-03-01 15:12:35 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,18 +12,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	i = 0;
-	while (src[i] && i < n)
-	{
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-		i++;
-	}
-	{
-		while (dest[i])
-		{
-			dest[i] = '\0';
-			i++;
-		}
-	}
+	for (; i < n; i++)
+
 	return (dest);
 }
