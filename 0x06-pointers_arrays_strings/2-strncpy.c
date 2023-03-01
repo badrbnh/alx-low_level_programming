f161aff3801db140042061e0ed8430a753f212a6
badreddine <badrechahide@gmail.com>
2023-03-01 14:44:42 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *_strncpy - copies a string
+ *@dest: destination
+ *@src: source
+ *@n: is the maximum number of characters to be copied
+ *Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	if (src[i] < n)
+	{
+
+		while (src[i])
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
