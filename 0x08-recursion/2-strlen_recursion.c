57a5befa6af45ff717ac7af422c6c61da63059e8
badreddine <badrechahide@gmail.com>
2023-03-08 14:14:05 -0800

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "maib.h"
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: Pointer
+ * Return: length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*str == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + string_length(str + 1));
+	}
+}
